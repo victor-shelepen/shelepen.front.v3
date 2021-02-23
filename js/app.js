fe3deccda865b4d79d9e05c77824e3c4ec4615bd
Victor Shelepen <victor.shelepen@gmail.com>
2021-02-23 15:26:02 +0200

    Google id was upfated, contact page was edited.

--- js/app.js
+++ js/app.js
@@ -31,5 +31,5 @@ $(document).ready(() => {
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
-ga('create', 'UA-46661212-1', 'auto');
+ga('create', '251268300', 'auto');
 ga('send', 'pageview');
